7b7c09e3848a1d9ec2860c9056e00c8bdff37fc6
Cilyan Olowen <gaknar@gmail.com>
2018-02-24 14:15:05 +0100

    Remove "Continue Reading" from index page
    There is already a "Read More" link on the layout.

--- src/php/functions.php
+++ src/php/functions.php
@@ -191,12 +191,18 @@ function t3p_excerpt_more( $link ) {
     return $link;
   }
 
-  $link = sprintf(
-    '<p class="link-more"><a href="%1$s" class="more-link">%2$s</a></p>',
-    esc_url( get_permalink( get_the_ID() ) ),
-    /* translators: %s: Name of current post */
-    sprintf( __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 't3p' ), get_the_title( get_the_ID() ) )
-  );
+  if ( is_home() ) {
+    $link = "";
+  }
+  else {
+    $link = sprintf(
+      '<p class="link-more"><a href="%1$s" class="more-link">%2$s</a></p>',
+      esc_url( get_permalink( get_the_ID() ) ),
+      /* translators: %s: Name of current post */
+      sprintf( __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 't3p' ), get_the_title( get_the_ID() ) )
+    );
+  }
+  
   return ' &hellip; ' . $link;
 }
 add_filter( 'excerpt_more', 't3p_excerpt_more' );
